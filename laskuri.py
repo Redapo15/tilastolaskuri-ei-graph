1c32a5b67d9815ec7517729482e7a17f00bac7ac
Redapo <69197418+Redapo15@users.noreply.github.com>
2023-11-28 18:10:36 +0200

    Update laskuri.py

--- laskuri.py
+++ laskuri.py
@@ -4,10 +4,8 @@ while i == 1:
     h = input()
     h = int(h)
   
-    if h > 48:
-        hinta = 3 + 2**(1/6.5*int(32))+2**(1/6*(int(48)-25)) + (h-48)
-    elif h >=32:
-        hinta = 3 + 2**(1/6.5*int(32))+2**(1/6*(int(h)-25))
+    if h >=31:
+        hinta = 3 + 2**(1/6.5*int(32)) + (h-32)
     elif h >= 10:
         hinta = 4 + 2**(1/6.5*int(h))
     elif h < 10:
